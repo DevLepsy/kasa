965831cc9f64d638a90bb45faadb7263b3d3ca4d
AdLepsy <henryck.paris@viacesi.fr>
2023-12-19 15:18:30 +0100

    Add navigation links to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/color_logo.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <img src={logo} alt="Kasa Logo" className="header-logo" />
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "nav-link-active" : "nav-link"
+              }
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/a-propos"
+              className={({ isActive }) =>
+                isActive ? "nav-link-active" : "nav-link"
+              }
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
