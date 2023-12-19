4dd55941b54d0b4f24a3abf682bdd18076cca382
AdLepsy <henryck.paris@viacesi.fr>
2023-12-19 17:15:23 +0100

    Add dynamic banner images and text based on current path

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import homePicture from "../assets/home_picture.jpg";
+import aboutPicture from "../assets/about_picture.jpg";
+
+const bannerImages = {
+  "/": {
+    image: homePicture,
+    text: "Chez vous, partout et ailleurs",
+  },
+  "/a-propos": {
+    image: aboutPicture,
+    text: "",
+  },
+};
 
 function Banner() {
-  return <div>Banner</div>;
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const bannerContent = bannerImages[currentPath] || bannerImages["/"];
+  return (
+    <section
+      className={`banner ${currentPath === "/" ? "" : "heigthAboutBanner"}`}
+    >
+      <img src={bannerContent.image} alt="Bannière" />
+      {currentPath === "/" && <h1>{bannerContent.text}</h1>}
+    </section>
+  );
 }
 
 export default Banner;
