5aea9d10c80b529c0ccff7f59963cac1f69e64e5
AdLepsy <henryck.paris@viacesi.fr>
2023-12-19 17:15:31 +0100

    Add banner and cards to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import Banner from "../components/Banner.jsx";
+import Card from "../components/Cards.jsx";
+import data from "../data/data.json";
 
 function Home() {
-  return <div>Home</div>;
+  const location = useLocation();
+  const cardsToShow = data.slice(0, 6);
+  return (
+    <div>
+      <Banner
+        // @ts-ignore
+        location={location}
+      />
+      <section className="cardsContainer">
+        {cardsToShow.map((item, index) => (
+          // @ts-ignore
+          <Card key={index} details={item} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default Home;
