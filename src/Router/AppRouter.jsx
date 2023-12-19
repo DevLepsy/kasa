1c12644fe8b4df78b55f79b4f165920dce9d8bd4
AdLepsy <henryck.paris@viacesi.fr>
2023-12-19 14:43:45 +0100

    Add routes and components to AppRouter

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Error from "../pages/Error";
 
 function AppRouter() {
   return <div>AppRouter</div>;
