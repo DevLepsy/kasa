f85916769a563bf222e23ff6ff4737f28ba0c67c
AdLepsy <henryck.paris@viacesi.fr>
2023-12-21 18:19:58 +0100

    Add Gallery component

---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// @ts-ignore
+import styles from "./Gallery.module.scss";
+
+function Gallery() {
+  return <div className={styles.gallery}>Gallery</div>;
+}
+
+export default Gallery;
