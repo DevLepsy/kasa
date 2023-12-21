cc7cfd5768d1c9d4b987c28fb1b339ca9d6d6ecb
AdLepsy <henryck.paris@viacesi.fr>
2023-12-21 18:15:30 +0100

    Refactor Home component to use Gallery component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+// @ts-ignore
+import styles from "./Home.module.scss";
 import Banner from "../../components/banner/Banner.jsx";
-import Card from "../../components/card/Cards.jsx";
-import data from "../../data/data.json";
+import Gallery from "../../components/gallery/Gallery.jsx";
 
 function Home() {
-  const location = useLocation();
-  const cardsToShow = data.slice(0, 6);
   return (
-    <div>
-      <Banner
-        // @ts-ignore
-        location={location}
-      />
-      <section className="cardsContainer">
-        {cardsToShow.map((item, index) => (
-          // @ts-ignore
-          <Card key={index} details={item} />
-        ))}
-      </section>
+    <div className={styles.home}>
+      <Banner />
+      <Gallery />
     </div>
   );
 }
-
 export default Home;
