cf312f1fd61eb6240aba8c232a2a6f9743fc3194
AdLepsy <henryck.paris@viacesi.fr>
2023-12-21 18:15:22 +0100

    Update import paths for pages in AppRouter.jsx

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Housing from "../pages/Housing";
-import Error from "../pages/Error";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Housing from "../pages/housing/Housing";
+import Error from "../pages/error/Error";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 
