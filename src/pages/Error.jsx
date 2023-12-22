0d250614c6ea8a93d013c7b8476ae56211a3d48e
AdLepsy <henryck.paris@viacesi.fr>
2023-12-22 15:57:57 +0100

    Refactor Error component to display a custom 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups ! La page que vous demandez n'existe pas.</p>
+      <a href="/">Retournez sur la page d'accueil.</a>
+    </div>
+  );
 }
 
 export default Error;
