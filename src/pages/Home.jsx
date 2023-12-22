788d2a6cbd373d9257df683639cfad65335ef283
AdLepsy <henryck.paris@viacesi.fr>
2023-12-22 10:38:33 +0100

    Add Home component with banner and card sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { useLocation } from "react-router-dom";
+import Banner from "../components/Banner.jsx";
+import Card from "../components/Card.jsx";
+import data from "../data/data.json";
+
+function Home() {
+  const location = useLocation();
+
+  return (
+    <div>
+      <Banner location={location} />
+      <section className="cardsContainer">
+        {data.map((location, index) => (
+          <Card location={location} key={index} />
+        ))}
+      </section>
+    </div>
+  );
+}
+export default Home;
