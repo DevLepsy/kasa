98faa5f0124abeb7dba9b6328cd5b595f8599a3d
AdLepsy <henryck.paris@viacesi.fr>
2023-12-22 10:37:25 +0100

    Add new components: Carrousel, Collapse, Footer, and Header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import white_logo from "../assets/white_logo.png";
+
+function Footer() {
+  return (
+    <footer>
+      <img src={white_logo} alt="Logo" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import color_logo from "../assets/color_logo.png"; // Assure-toi que le chemin vers tes images est correct.
+
+export default function Header() {
+  return (
+    <nav>
+      <img src={color_logo} alt="Logo Kasa" />
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/a-propos"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            A Propos
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
