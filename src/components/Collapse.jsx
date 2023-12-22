9c9bde392138d35a000f0889e054b49326455b36
AdLepsy <henryck.paris@viacesi.fr>
2023-12-22 12:57:00 +0100

    Add collapse functionality to Collapse component

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+// @ts-ignore
+import arrow_down from "../assets/arrow_down.png";
+// @ts-ignore
+import arrow_up from "../assets/arrow_up.png";
 
-function Collapse() {
-  return <div>Collapse</div>;
+export default function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  const handleToggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapseContainer">
+      <div
+        onClick={handleToggleCollapse}
+        className={`collapse ${isOpen ? "open" : ""}`}
+      >
+        <p>{title}</p>
+        <img
+          src={isOpen ? arrow_down : arrow_up}
+          alt={isOpen ? "fermer" : "ouvrir"}
+        />
+      </div>
+      {isOpen && (
+        <div ref={contentRef} className="collapsibleContent">
+          <ul>{content}</ul>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default Collapse;
