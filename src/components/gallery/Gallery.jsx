855135379a806617f6bbfc6997372d66bd4e687e
AdLepsy <henryck.paris@viacesi.fr>
2023-12-29 11:55:58 +0100

    Delete Gallery component and its associated styles

--- src/components/gallery/Gallery.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// @ts-ignore
-import styles from "./Gallery.module.scss";
-
-function Gallery() {
-  return <div className={styles.gallery}>Gallery</div>;
-}
-
-export default Gallery;
